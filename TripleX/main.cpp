0b841e49e799d6f03e00fa39de40a39b5933a777
Paras Bhandari <50918320+gamedev-parash@users.noreply.github.com>
2022-07-11 18:15:45 +0545

    Using Rand

--- TripleX/main.cpp
+++ TripleX/main.cpp
@@ -5,41 +5,46 @@ void PrintIntrouction(int defficulty)
 {
 	cout << "You are a secret agent breaking into a secure server room\n";
 	cout << "You need to enter the correct codes to continue...";
-	cout << "your difficulty level increase as you pass:" << defficulty;
+	cout << "your difficulty level increase as you pass: " << defficulty<<endl;
 }
 bool PlayGame(int levelsdefficulty)
 {
 	PrintIntrouction(levelsdefficulty);
-	int GussesA, GussesB, GussesC;
+	int GussesA = rand()%1+levelsdefficulty+1, GussesB = rand()%1+levelsdefficulty+1, GussesC = rand()%1+levelsdefficulty+1;
+	int CodeVal = GussesA + GussesB + GussesC;
+	int ProductVal = GussesA * GussesB * GussesC;
+	
+	cout << "Enter any three Number in the Code";
+	cout << "\n The Code are added up to :" << CodeVal;
+	cout << "\n THe Code Multiply to give :" << ProductVal <<endl;
 	cin >> GussesA >> GussesB >> GussesC;
-
 	int GussesSum = GussesA + GussesB + GussesC;
 	int GussesProduct = GussesA * GussesB * GussesC;
-	if (GussesSum < GussesProduct)
+	if (CodeVal == GussesSum && ProductVal ==GussesProduct)
 	{
-		cout << "you Won!";
-		return true;
+		cout << "you won";
+			return true;
 	}
 	else
 	{
 		cout << "you lose";
 		return false;
 	}
-	
 }
 int main()
 {
-	while (true)
-	{	int leveldefeculty = 1;
-		bool bLevelGame = PlayGame(leveldefeculty);
-	
-
-		if (bLevelGame)
+	int minlevel = 1;
+	int Maxlevel = 5;
+	int levelDifficluty = 1;
+	while (minlevel<=Maxlevel)
+	{
+		bool bLevelCompleted = PlayGame(levelDifficluty);
+		if (bLevelCompleted)
 		{
-			leveldefeculty += 1;
+			++levelDifficluty;
 		}
-
+		minlevel++;
+		return 0;
 	}
-	return 0;
-
-}
+	}
+	
